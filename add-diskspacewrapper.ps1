3f3b240a6e677d2f16a0cb2f3f840ab861baa826
PaulHenry678 <45956263+PaulHenry678@users.noreply.github.com>
2018-12-26 16:09:55 -0500

    Added Wrapper for multiple servers to expand at once

---
+++ add-diskspacewrapper.ps1
@@ -0,0 +1,10 @@
+﻿#$servers = import-csv -Path C:\temp\serverstoexpand.csv
+$servers = (import-csv -Path C:\temp\serverstoexpand.csv) | select -ExpandProperty servername
+$total = $servers | measure | select -expandproperty count
+$i = 1
+
+foreach ($server in $servers) {
+Write-Host -ForegroundColor DarkYellow  "On $server, $i of $total"
+Add-DiskSpace $server
+$i++
+}
